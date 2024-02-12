c4999a84cf913c40a252a857b2e054be1ef4421c
Donnukrit Satirakul <kalima007c@gmail.com>
2024-02-12 17:35:55 +0700

    Set Write Channel Done!

--- app/src/main/java/com/exvention/kotlin_bt_service/BluetoothService.kt
+++ app/src/main/java/com/exvention/kotlin_bt_service/BluetoothService.kt
@@ -5,24 +5,34 @@ import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothManager
 import android.bluetooth.BluetoothProfile
 import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat.getSystemService
 import java.util.UUID
 
 
 class BluetoothService {
     private lateinit var bluetoothAdapter: BluetoothAdapter
     private lateinit var bluetoothLeScanner: BluetoothLeScanner
+    private lateinit var bluetoothGatt: BluetoothGatt
     private val devices = mutableListOf<BluetoothDevice>()
- init {
-     Log.d("Init BluetoothService", "")
-     bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-     bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
- }
+    private lateinit var SERVICE_UUID: UUID
+    private lateinit var CHARACTERISTIC_UUID: UUID
+
+    init {
+        Log.d("Init BluetoothService", "")
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+    }
+
     @SuppressLint("MissingPermission")
     fun scanLeDevice(enable: Boolean) {
         if (enable) {
@@ -43,31 +53,70 @@ class BluetoothService {
     }
 
     @SuppressLint("MissingPermission")
-    private fun connectToDevice(context: Context, device: BluetoothDevice) {
+    fun connectToDevice(context: Context, device: BluetoothDevice) {
         device.connectGatt(context, false, object : BluetoothGattCallback() {
             override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
                 if (newState == BluetoothProfile.STATE_CONNECTED) {
-                    gatt.discoverServices()
+                    Log.d("onConnectionStateChange", "Connected to ${device.address}")
+                    bluetoothGatt = gatt
+                    val result = gatt.discoverServices()
+                    Log.d("discoverServices", "result: $result")
                 }
             }
-
             override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
-                // Here you can get the services and characteristics for the device
+                val targetChannel : Int = 26
+                Log.d("Service", "${gatt.services.size} services discovered.")
+                gatt.services.forEach { service ->
+                    service.characteristics.forEach { characteristic ->
+                        Log.d("onServicesDiscovered", "service: ${service.uuid}")
+                        Log.d("onServicesDiscovered", "characteristic: ${characteristic.uuid}")
+                    }
+                }
+                gatt.services.forEach { service ->
+                    service.characteristics.forEach { characteristic ->
+                        if (characteristic.properties == targetChannel) {
+                            setWritableChannel(service.uuid, characteristic.uuid)
+                            return
+                        }
+                    }
+                }
             }
         })
     }
 
-    @SuppressLint("MissingPermission")
-    private fun sendCharacteristic(
-        gatt: BluetoothGatt,
-        serviceUUID: UUID,
-        characteristicUUID: UUID,
+    private fun setWritableChannel(serviceUUID: UUID, characteristicUUID: UUID) {
+        Log.d("setWritableChannel", "serviceUUID: $serviceUUID | $characteristicUUID")
+        SERVICE_UUID = serviceUUID
+        CHARACTERISTIC_UUID = characteristicUUID
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    @SuppressLint("MissingPermission", "WrongConstant")
+    fun sendCharacteristic(
         value: ByteArray
     ) {
-        val service = gatt.getService(serviceUUID)
-        val characteristic = service.getCharacteristic(characteristicUUID)
-        characteristic.value = value
-        gatt.writeCharacteristic(characteristic)
+
+//        Log.d("sendCharacteristic", "value: $value")
+//        Log.d("sendCharacteristic", "serviceUUID: $SERVICE_UUID")
+//        Log.d("sendCharacteristic", "characteristicUUID: $CHARACTERISTIC_UUID")
+//        Log.d("sendCharacteristic", "service count : ${bluetoothGatt.services.size}")
+//        Log.d(
+//            "sendCharacteristic",
+//            "char count : ${bluetoothGatt.services[0].characteristics.size}"
+//        )
+
+        val service = bluetoothGatt.getService(SERVICE_UUID)
+        val characteristic = service.getCharacteristic(CHARACTERISTIC_UUID)
+
+        if (characteristic != null) {
+
+            Log.d("sendCharacteristic", "characteristic prop: ${characteristic.properties}")
+            characteristic.setValue(value);
+            characteristic.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+            var result = bluetoothGatt.writeCharacteristic(characteristic)
+            Log.d("sendCharacteristic", "result: $result")
+        }
+
     }
 
 }
--- app/src/main/java/com/exvention/kotlin_bt_service/MainActivity.kt
+++ app/src/main/java/com/exvention/kotlin_bt_service/MainActivity.kt
@@ -1,22 +1,80 @@
 package com.exvention.kotlin_bt_service
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothProfile
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import java.util.UUID
 
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bluetoothService : BluetoothService = BluetoothService()
+        val bluetoothService: BluetoothService = BluetoothService()
 
 
-        var scanButton : Button = findViewById<Button>(R.id.scan_btn)
+        var scanButton: Button = findViewById<Button>(R.id.scan_btn)
         scanButton.setOnClickListener {
             bluetoothService.scanLeDevice(true)
         }
 
+        var connectButton: Button = findViewById<Button>(R.id.connect_btn)
+        connectButton.setOnClickListener {
+            val bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+            val deviceAddress = "40:4C:CA:4A:C9:42"
+            var device: BluetoothDevice? = null
+
+            if (BluetoothAdapter.checkBluetoothAddress(deviceAddress)) {
+                device = bluetoothAdapter.getRemoteDevice(deviceAddress)
+            }
+
+            if (device != null) {
+                bluetoothService.connectToDevice(this, device)
+            }
+        }
+
+        var sendButton: Button = findViewById<Button>(R.id.send_btn)
+        sendButton.setOnClickListener {
+            val bluetoothAdapter: BluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+            val deviceAddress = "40:4C:CA:4A:C9:42"
+            var device: BluetoothDevice? = null
+
+            if (BluetoothAdapter.checkBluetoothAddress(deviceAddress)) {
+                device = bluetoothAdapter.getRemoteDevice(deviceAddress)
+            }
+            Log.d("BTN", "ON SEND")
+
+            val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+            val state = bluetoothManager.getConnectionState(device, BluetoothProfile.GATT)
+            Log.d("state", "$state")
+            if (state == BluetoothProfile.STATE_CONNECTED) {
+                Log.d("BT", "CONNECTED")
+            }
+            Log.d("BTN", "Sending Characteristic")
+            bluetoothService.sendCharacteristic("TESTSEND".toByteArray())
+
+
+        }
+
     }
 
-
+    companion object {
+        val SERVICE_UUID: UUID = UUID.fromString("00001801-0000-1000-8000-00805f9b34fb")
+        val CHARACTERISTIC_UUID: UUID = UUID.fromString("00002a01-0000-1000-8000-00805f9b34fb")
+        val DATA: ByteArray = "TEST_DATA".toByteArray()
+    }
 }
