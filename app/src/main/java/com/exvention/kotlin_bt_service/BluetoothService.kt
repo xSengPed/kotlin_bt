5b967cafe11a21614d61708bb4dcebc1fb715eaf
Donnukrit Satirakul <kalima007c@gmail.com>
2024-02-12 17:43:32 +0700

    Set Write Channel Done!

--- app/src/main/java/com/exvention/kotlin_bt_service/BluetoothService.kt
+++ app/src/main/java/com/exvention/kotlin_bt_service/BluetoothService.kt
@@ -1,5 +1,6 @@
 package com.exvention.kotlin_bt_service
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
@@ -12,9 +13,11 @@ import android.bluetooth.le.BluetoothLeScanner
 import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.getSystemService
 import java.util.UUID
 
@@ -52,36 +55,48 @@ class BluetoothService {
         }
     }
 
-    @SuppressLint("MissingPermission")
     fun connectToDevice(context: Context, device: BluetoothDevice) {
-        device.connectGatt(context, false, object : BluetoothGattCallback() {
-            override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
-                if (newState == BluetoothProfile.STATE_CONNECTED) {
-                    Log.d("onConnectionStateChange", "Connected to ${device.address}")
-                    bluetoothGatt = gatt
-                    val result = gatt.discoverServices()
-                    Log.d("discoverServices", "result: $result")
-                }
-            }
-            override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
-                val targetChannel : Int = 26
-                Log.d("Service", "${gatt.services.size} services discovered.")
-                gatt.services.forEach { service ->
-                    service.characteristics.forEach { characteristic ->
-                        Log.d("onServicesDiscovered", "service: ${service.uuid}")
-                        Log.d("onServicesDiscovered", "characteristic: ${characteristic.uuid}")
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.BLUETOOTH_CONNECT
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            device.connectGatt(context, false, object : BluetoothGattCallback() {
+                @SuppressLint("MissingPermission")
+                override fun onConnectionStateChange(
+                    gatt: BluetoothGatt,
+                    status: Int,
+                    newState: Int
+                ) {
+                    if (newState == BluetoothProfile.STATE_CONNECTED) {
+                        Log.d("onConnectionStateChange", "Connected to ${device.address}")
+                        bluetoothGatt = gatt
+                        val result = gatt.discoverServices()
+                        Log.d("discoverServices", "result: $result")
                     }
                 }
-                gatt.services.forEach { service ->
-                    service.characteristics.forEach { characteristic ->
-                        if (characteristic.properties == targetChannel) {
-                            setWritableChannel(service.uuid, characteristic.uuid)
-                            return
+
+                override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+                    val targetChannel: Int = 26
+                    Log.d("Service", "${gatt.services.size} services discovered.")
+                    gatt.services.forEach { service ->
+                        service.characteristics.forEach { characteristic ->
+                            Log.d("onServicesDiscovered", "service: ${service.uuid}")
+                            Log.d("onServicesDiscovered", "characteristic: ${characteristic.uuid}")
+                        }
+                    }
+                    gatt.services.forEach { service ->
+                        service.characteristics.forEach { characteristic ->
+                            if (characteristic.properties == targetChannel) {
+                                setWritableChannel(service.uuid, characteristic.uuid)
+                                return
+                            }
                         }
                     }
                 }
-            }
-        })
+            })
+        }
+
     }
 
     private fun setWritableChannel(serviceUUID: UUID, characteristicUUID: UUID) {
@@ -90,21 +105,11 @@ class BluetoothService {
         CHARACTERISTIC_UUID = characteristicUUID
     }
 
-    @RequiresApi(Build.VERSION_CODES.P)
+
     @SuppressLint("MissingPermission", "WrongConstant")
     fun sendCharacteristic(
         value: ByteArray
     ) {
-
-//        Log.d("sendCharacteristic", "value: $value")
-//        Log.d("sendCharacteristic", "serviceUUID: $SERVICE_UUID")
-//        Log.d("sendCharacteristic", "characteristicUUID: $CHARACTERISTIC_UUID")
-//        Log.d("sendCharacteristic", "service count : ${bluetoothGatt.services.size}")
-//        Log.d(
-//            "sendCharacteristic",
-//            "char count : ${bluetoothGatt.services[0].characteristics.size}"
-//        )
-
         val service = bluetoothGatt.getService(SERVICE_UUID)
         val characteristic = service.getCharacteristic(CHARACTERISTIC_UUID)
 
